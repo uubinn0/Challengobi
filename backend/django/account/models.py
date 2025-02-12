85ae0af9cf8bf79643c9d53ced82f5aa93097068
나유빈 <ub.devl0908@gmail.com>
2025-02-12 09:28:48 +0900

    refactor: 패키지 수정

--- backend/django/account/models.py
+++ backend/django/account/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.contrib.auth.base_user import BaseUserManager
 from django.utils.translation import gettext_lazy as _
 from django.core.exceptions import ValidationError
